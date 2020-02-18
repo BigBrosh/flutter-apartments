0484e0c65cc98c18dd903f4d40c149d042aa41be
BigBrosh <maksuhin@gmail.com>
2020-02-18 21:07:03 +0200

    - added implicit listingsService.

--- lib/views/HomePage/HomePage.dart
+++ lib/views/HomePage/HomePage.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:nestoria_appartments/services/ListingsService/ListingsService.dart';
 import 'package:nestoria_appartments/stores/Listings/Listings.dart';
 import 'package:nestoria_appartments/widgets/SearchForm/SearchForm.dart';
 import 'package:nestoria_appartments/widgets/SearchHeader/SearchHeader.dart';
 
-final listings = Listings();
+final listingsService = new ListingsService();
+final listings = new Listings(listingsService);
 
 class HomePage extends StatelessWidget {
   @override
