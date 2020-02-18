78eb8e5e031723a10d8551d361209d6e7552d3e7
BigBrosh <maksuhin@gmail.com>
2020-02-18 20:01:08 +0200

    - modified ListingsService.

--- lib/services/ListingsService/ListingsService.dart
+++ lib/services/ListingsService/ListingsService.dart
@@ -4,20 +4,29 @@ import 'package:nestoria_appartments/initializations/initDio.dart';
 import 'package:nestoria_appartments/services/ListingsService/ListingsServiceInterface.dart';
 
 class ListingsService implements ListingsServiceInterface {
+  final String url = 'http://api.nestoria.co.uk/api';
+  final Map<String, dynamic> basicParams = {
+      'country': 'uk',
+      'pretty': 1,
+      'action': 'search_listings',
+      'encoding': 'json',
+      'listing_type': 'buy',
+      'page': 1
+  };
+
   Future<List<dynamic>> getBySearchValue(String searchValue) {
-    final url = 'http://api.nestoria.co.uk/api?country=uk&pretty=1&action=search_listings&encoding=json&listing_type=buy&page=1&place_name=$searchValue';
-    return fetchByUrl(url);
+    return fetchByUrl({ 'place_name': searchValue });
   }
 
   Future<List<dynamic>> getByLocation(LocationData currentLocation) {
     String latLng = '${currentLocation.latitude},${currentLocation.longitude}';
-    final url = 'http://api.nestoria.co.uk/api?country=uk&pretty=1&action=search_listings&encoding=json&listing_type=buy&page=1&centre_point=$latLng';
-
-    return fetchByUrl(url);
+    return fetchByUrl({ 'centre_point': latLng });
   }
 
-  Future<List<dynamic>> fetchByUrl(String url) async {
-    final response = await dio.get(url);
+  Future<List<dynamic>> fetchByUrl(Map<String, dynamic> additional) async {
+    final Map<String, dynamic> queryParameters = new Map.from(basicParams)..addAll(additional);
+    final response = await dio.get(url, queryParameters: queryParameters);
+
     return json.decode(response.data)['response']['listings'];
   }
 }
